6e9605b98b11fbaee248c5cbb1229ee45cbb59d1
Artem <marsov78@mail.ru>
2020-06-25 20:14:23 +0300

    Release 1

---
+++ public/js/animeIndex.js
@@ -0,0 +1,15 @@
+// const anime = require('animejs');
+
+// var animateLeft = anime({
+//   targets: '.square',
+//   left: '100%',
+//   autoplay: false
+// });
+
+let animation = anime({
+  targets: '.square',
+  translateX: 130,
+  duration: 1500,
+  easing: 'linear',
+  direction: 'linear',
+});  
