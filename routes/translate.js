f4893bf09582907d16650dde005b5408af5cd252
Sergey <boma385@gmail.com>
2020-06-26 14:21:17 +0300

    smth2

--- routes/translate.js
+++ routes/translate.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   try {
+    
     req.session.currentLang = req.query.lang;
     // console.log('req.session.currentLang', req.session.currentLang);
     const hbs = req.headers.referer.slice(22);
