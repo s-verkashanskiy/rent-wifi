172cebaed3cda1d313014bcc0c9354ab7b9fafb2
Artem <marsov78@mail.ru>
2020-06-26 14:06:54 +0300

    footer

--- public/js/animeIndex.js
+++ public/js/animeIndex.js
@@ -6,26 +6,26 @@
 //   autoplay: false
 // });
 
-let animation = anime({
-  targets: '.text1',
-  translateX: 130,
-  duration: 700,
-  easing: 'linear',
-  direction: 'linear',
-});
+// let animation = anime({
+//   targets: '.text1',
+//   translateX: 130,
+//   duration: 700,
+//   easing: 'linear',
+//   direction: 'linear',
+// });
 
-animation = anime({
-  targets: '.text2',
-  translateX: -130,
-  duration: 700,
-  easing: 'linear',
-  direction: 'linear',
-});
+// animation1 = anime({
+//   targets: '.text2',
+//   translateX: 130,
+//   duration: 700,
+//   easing: 'linear',
+//   direction: 'linear',
+// });
 
-animation = anime({
-  targets: '.img1',
-  translateX: 0,
-  duration: 700,
-  easing: 'linear',
-  direction: 'linear',
-});
+// animation2 = anime({
+//   targets: '.img1',
+//   translateX: 0,
+//   duration: 700,
+//   easing: 'linear',
+//   direction: 'linear',
+// });
